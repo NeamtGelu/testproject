f124007cf5f24ec7534f719b353c07040cecc7b0
Gelu Neamt <gelu.neamt@softvision.ro>
2016-05-12 16:31:29 +0300

    Console Project version 3.0

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-Here are instructions for this project!
+Hello !
+
+Welcome to Console Project Edmunds API
+
+Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+Get this project from GitHub using:
+
+git clone git@github.com:NeamtGelu/testproject.git
+
+After that, you can run project using line command.
+Open your Terminal and change directory to root project.
+
+cd /root/testproject
+
+To run application type
+
+php bin/index.php
+
+You will show a list of options like that:
+
+  [0] If you want get a list of vehicles models makes by year type "0"
+  [1] If you want get vehicle information makes by VIN type "1"
+  [2] If you want get a list of vehicles models by car nickname and year make type "2"
+
+Select what you want to do from list of options: 0, 1, or 2 and press ENTER.
+
+If you choise "0", next step is to insert YEAR in interval 1990 adn current year.
+
+After you press ENTER, if year is correct, will display a list of cars name make in year entered.
+Else, you will show a error message.
+
+For choise "1", you need to have a valid VIN vehicle that must be 17 characters and to paste it.
+Here you have a test VIN for test : 1N4AL3AP4DC295509 or 2G1FC3D33C9165616
+
+You will get all vehicle details from make, model, year, engine and other options
+
+And for last option "2" you can insert only car "nickname" to show a list of car models, or if you want to get car models for a specific "year" write that too.
+Exemple 1: audi
+Exemple 2 : audi 2010
+
+That's all !
+
+Thanke you for using this tool :)
--- src/GreetCommand.php
+++ src/GreetCommand.php
@@ -4,9 +4,11 @@ namespace Gelu;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
-//use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
 
 class GreetCommand extends Command
 {
@@ -16,20 +18,20 @@ class GreetCommand extends Command
             ->setName('car:option')
 
             ->setDescription('Cars operation :')
-            ->addArgument(
-                'option',
-                InputArgument::OPTIONAL,
-                'What do you want to greet?'
-            )
-            ->addArgument(
-                'type',
-                InputArgument::OPTIONAL,
-                'What do you want to search?'
-            )
-            ->addArgument(
-                'year',
-                InputArgument::OPTIONAL
-            )
+//            ->addArgument(
+//                'option',
+//                InputArgument::OPTIONAL,
+//                'What do you want to greet?'
+//            )
+//            ->addArgument(
+//                'type',
+//                InputArgument::OPTIONAL,
+//                'What do you want to search?'
+//            )
+//            ->addArgument(
+//                'year',
+//                InputArgument::OPTIONAL
+//            )
 //            ->addArgument(
 //                'vin',
 //                InputArgument::OPTIONAL,
@@ -52,66 +54,69 @@ class GreetCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if($input->getArgument('option') == '') {
-            $output->writeln('1.If you want get a list of vehicles models makes by year type "car:option 1" ');
-            $output->writeln('2.If you want get vehicle information makes by VIN type "car:option 2" ');
-            $output->writeln('3.If you want get a list of vehicles models by car nickname and year make type "car:option 3" ');
-        }
-        else{
-            $option = $input->getArgument('option');
-            $type = $input->getArgument('type');
-            $year = $input->getArgument('year');
 
-            if($type == ''){
-                if($option == '1') {
+        $helper = $this->getHelper('question');
+        $question = new ChoiceQuestion(
+            'Please select what operation you want to do',
+            array('If you want get a list of vehicles models makes by year type "0" ',
+                   'If you want get vehicle information makes by VIN type "1" ',
+                   'If you want get a list of vehicles models by car nickname and year make type "2" '),
+            0
+        );
+        $question->setErrorMessage('Invalid choise. Please insert a valid number');
 
-                    $output->writeln('Please insert "year" to get a list of vehicles models makes in selected year.');
+        $choise = $helper->ask($input, $output, $question);
+
+        $var = $question->getChoices();
+
+        $choise_key = array_search($choise, $var);
+
+//        $output->writeln('You have just selected: '.$choise_key);
+
+        switch($choise_key){
+            case '0':
+                $question = new Question('Please insert "year" to get a list of vehicles models makes in selected year.'."\n");
+
+                $year = $helper->ask($input, $output, $question);
+
+                if ($year > 1990 && $year <= date('Y')) {
+
+                    $cSession = curl_init();
+
+                    $url = "https://api.edmunds.com/api/vehicle/v2/makes?state=used&year={$year}&view=basic&fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
+                    curl_setopt($cSession, CURLOPT_URL, $url);
+                    curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
+
+                    $result = curl_exec($cSession);
+                    $json = json_decode($result);
+
+                    curl_close($cSession);
+
+                    foreach ($json->makes as $makes) {
+                        echo $makes->name . "\n";
+                    };
+
+                    $text = 'Here you have a list of ' . $json->makesCount . ' vehicles models makes in year "'.$year.'" ';
+
+                } else {
+                    $text = 'Please insert a valid "year" to get a list of vehicles. Year should be a four-digit number.';
                 }
-                elseif ($option == '2'){
 
-                    $output->writeln('Please enter the vehicle "VIN" to get all vehicle details from make, model, year, engine and other options.');
-                }
-                elseif ($option == '3'){
+                $output->writeln($text);
 
-                    $output->writeln('Please insert "car nickname and year" to get a list of all Models vehicle by its Make/Year'.
-                                "\n".'Exemple : audi / audi 2015');
-                }
-            }
-            else{
-                if($option == '1') {
+                break;
 
-                    if ($type > 1990 && $type <= date('Y')) {
+            case '1':
+
+                $question = new Question('Please enter the vehicle "VIN" to get all vehicle details from make, model, year, engine and other options'."\n");
+
+                    $vin = $helper->ask($input, $output, $question);
+
+                    if (strlen($vin) == 17) {
 
                         $cSession = curl_init();
 
-                        $url = "https://api.edmunds.com/api/vehicle/v2/makes?state=used&year={$type}&view=basic&fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
-                        curl_setopt($cSession, CURLOPT_URL, $url);
-                        curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
-
-                        $result = curl_exec($cSession);
-                        $json = json_decode($result);
-
-                        curl_close($cSession);
-
-                        foreach ($json->makes as $makes) {
-                            echo $makes->name . "\n";
-                        };
-
-                        $text = 'Here you have a list of ' . $json->makesCount . ' vehicles models makes in year "'.$type.'" ';
-
-                    } else {
-                        $text = 'Please insert a valid "year" to get a list of vehicles. Year should be a four-digit number.';
-                    }
-
-                    $output->writeln($text);
-                }
-                elseif ($option == '2'){
-
-                    if (strlen($type) == 17) {
-
-                        $cSession = curl_init();
-
-                        $url = "https://api.edmunds.com/api/vehicle/v2/vins/{$type}?fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
+                        $url = "https://api.edmunds.com/api/vehicle/v2/vins/{$vin}?fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
                         curl_setopt($cSession, CURLOPT_URL, $url);
                         curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
 
@@ -126,7 +131,7 @@ class GreetCommand extends Command
                         }
                         else{
 
-                            $text = "\n" . 'Vehicle details for VIN: ' . $type . ':' .
+                            $text = "\n" . 'Vehicle details for VIN: ' . $vin . ':' .
                                 (isset($json->make->name) ? "\n" . 'Make: ' . $json->make->name : '') .
                                 (isset($json->model->name) ? "\n" . 'Model: ' . $json->model->name : '') .
                                 (isset($json->engine->size) ? "\n" . 'Engine size: ' . $json->engine->size : '') .
@@ -144,207 +149,78 @@ class GreetCommand extends Command
 
                     $output->writeln($text);
 
+                break;
+
+            case '2':
+
+                $question = new Question('Please insert "car nickname and year" to get a list of all Models vehicle by its Make/Year'.
+                    "\n".'Exemple : audi / audi 2015'."\n");
+
+                $data = $helper->ask($input, $output, $question);
+
+                $data = explode(' ', $data);
+
+                $nickname = $data[0];
+
+                if(isset($data[1])) {
+                    $year = $data[1];
                 }
-                elseif ($option == '3'){
+                else $year = '';
 
-                    if ($year) {
+                if ($year !='') {
 
-                        $url_type = "https://api.edmunds.com/api/vehicle/v2/{$type}/models?year={$year}&view=basic&fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
+                    $url_type = "https://api.edmunds.com/api/vehicle/v2/{$nickname}/models?year={$year}&view=basic&fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
 
-                        $text_type = 'Here you have a list of car models for car nickname: "' . $type . '" make in year "' . $year . '" :';
+                    $text_type = 'Here you have a list of car models for car nickname: "' . $nickname . '" make in year "' . $year . '" :';
 
-                        $text_error = 'No results for make vehicle "'.$type.'" make in year "' . $year . '" !';
-
-                    } else {
-
-                        $url_type = "https://api.edmunds.com/api/vehicle/v2/{$type}/models?view=basic&fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
-
-                        $text_type = 'Here you have a list of car models for car nickname: "' . $type . '" ';
-
-                        $text_error = 'No results for make vehicle "'.$type.'" !';
-
-                    }
-                    $cSession = curl_init();
-
-                    $url = $url_type;
-                    curl_setopt($cSession, CURLOPT_URL, $url);
-                    curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
-
-                    $result = curl_exec($cSession);
-                    $json = json_decode($result);
-
-                    curl_close($cSession);
-
-                    $list_vehicles = '';
-
-                    if (!empty($json->models)) {
-
-                        foreach ($json->models as $models) {
-
-                            foreach ($models->years as $years_list) {
-
-                                foreach ($years_list->styles as $style) {
-
-                                    $list_vehicles .= "\n " . $type . " -> " . $models->name . " -> " . $years_list->year . " -> " . $style->name;
-                                }
-                            }
-                        };
-
-                        $text = $list_vehicles . "\n" . $text_type;
-                    }
-                    else{
-                        $text = "\n" . $text_error;
-                    }
-
-                    $output->writeln($text);
-                }
-            }
-
-        }
-
-//        $year = $input->getArgument('year');
-//        $vin = $input->getArgument('vin');
-//        $make = $input->getArgument('make');
-
-/*
-        if($year) {
-
-            if($year != '-') {
-
-
-                if ($year > 1990 && $year <= date('Y')) {
-
-                    $cSession = curl_init();
-
-                    $url = "https://api.edmunds.com/api/vehicle/v2/makes?state=used&year={$year}&view=basic&fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
-                    curl_setopt($cSession, CURLOPT_URL, $url);
-                    curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
-
-                    $result = curl_exec($cSession);
-                    $json = json_decode($result);
-
-                    curl_close($cSession);
-
-                    //print_r($json);
-
-                    foreach ($json->makes as $makes) {
-                        echo $makes->name . "\n";
-                    };
-
-                    $text = 'Here you have a list of ' . $json->makesCount . ' vehicles models makes in year "'.$year.'" ';
+                    $text_error = 'No results for make vehicle "'.$nickname.'" make in year "' . $year . '" !';
 
                 } else {
-                    $text = 'Please insert a valid "year" to get a list of vehicles. Year should be a four-digit number.';
+
+                    $url_type = "https://api.edmunds.com/api/vehicle/v2/{$nickname}/models?view=basic&fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
+
+                    $text_type = 'Here you have a list of car models for car nickname: "' . $nickname . '" ';
+
+                    $text_error = 'No results for make vehicle "'.$nickname.'" !';
+
                 }
+                $cSession = curl_init();
 
+                $url = $url_type;
+                curl_setopt($cSession, CURLOPT_URL, $url);
+                curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
 
-                if ($input->getOption('yell')) {
-                    $text = strtoupper($text);
+                $result = curl_exec($cSession);
+                $json = json_decode($result);
+
+                curl_close($cSession);
+
+                $list_vehicles = '';
+
+                if (!empty($json->models)) {
+
+                    foreach ($json->models as $models) {
+
+                        foreach ($models->years as $years_list) {
+
+                            foreach ($years_list->styles as $style) {
+
+                                $list_vehicles .= "\n " . $nickname . " -> " . $models->name . " -> " . $years_list->year . " -> " . $style->name;
+                            }
+                        }
+                    };
+
+                    $text = $list_vehicles . "\n" . $text_type;
+                }
+                else{
+                    $text = "\n" . $text_error;
                 }
 
                 $output->writeln($text);
-            }
+
+                break;
+
         }
-
-
-                if($vin) {
-                    if ($vin != '-') {
-                        if (strlen($vin) == 17) {
-
-                            $cSession = curl_init();
-
-                            $url = "https://api.edmunds.com/api/vehicle/v2/vins/{$vin}?fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
-                            curl_setopt($cSession, CURLOPT_URL, $url);
-                            curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
-
-                            $result = curl_exec($cSession);
-                            $json = json_decode($result);
-
-                            curl_close($cSession);
-
-        //                print_r($json);
-
-                            $text = "\n" . 'Vehicle details for VIN: ' . $vin . ':' .
-                            (isset($json->make->name) ? "\n" . 'Make: ' . $json->make->name : '') .
-                            (isset($json->model->name) ? "\n" . 'Model: ' . $json->model->name : '') .
-                            (isset($json->engine->size) ? "\n" . 'Engine size: ' . $json->engine->size : '') .
-                            (isset($json->engine->horsepower) ? "\n" . 'Horse powe: ' . $json->engine->horsepower : '') .
-                            (isset($json->years->year) ? "\n" . 'Year: ' . $json->years->year : '') .
-                            (isset($json->numOfDoors) ? "\n" . 'Number of Doors: ' . $json->numOfDoors : '') .
-                            (isset($json->categories->vehicleSize) ? "\n" . 'Style: ' . $json->categories->vehicleSize : '') .
-                            (isset($json->categories->EPAClass) ? "\n" . 'Class: ' . $json->categories->EPAClass : '');
-
-                        } else {
-                            $text = 'The VIN is incorrect. It must be 17 characters.';
-                        }
-
-
-                        if ($input->getOption('yell')) {
-                            $text = strtoupper($text);
-                        }
-
-                        $output->writeln($text);
-
-                    }
-                }
-
-                if($make) {
-                    if ($make != '-') {
-                        if ($year != '-') {
-
-                            $url_type = "https://api.edmunds.com/api/vehicle/v2/{$make}/models?year={$year}&view=basic&fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
-
-                            $text_type = 'Here you have a list of car models for car nickname: "' . $make . '" make in year "' . $year . '" :';
-
-                            $text_error = 'No results for make vehicle "'.$make.'" make in year "' . $year . '" !';
-
-                        } else {
-
-                            $url_type = "https://api.edmunds.com/api/vehicle/v2/{$make}/models?view=basic&fmt=json&api_key=y2zqcuv5n9cnm4d2g8yw6p9w";
-
-                            $text_type = 'Here you have a list of car models for car nickname: "' . $make . '" ';
-
-                            $text_error = 'No results for make vehicle "'.$make.'" !';
-
-                        }
-                        $cSession = curl_init();
-
-                        $url = $url_type;
-                        curl_setopt($cSession, CURLOPT_URL, $url);
-                        curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
-
-                        $result = curl_exec($cSession);
-                        $json = json_decode($result);
-
-                        curl_close($cSession);
-
-                        //print_r($json->models);
-
-                        $list_vehicles = '';
-
-                        if (!empty($json->models)) {
-
-                            foreach ($json->models as $models) {
-
-                                foreach ($models->years as $years_list) {
-
-                                    foreach ($years_list->styles as $style) {
-
-                                        $list_vehicles .= "\n " . $make . " -> " . $models->name . " -> " . $years_list->year . " -> " . $style->name;
-                                    }
-                                }
-                            };
-
-                            $text = $list_vehicles . "\n" . $text_type;
-                        }
-                        else{
-                            $text = "\n" . $text_error;
-                        }
-
-                        $output->writeln($text);
-
-                    }
-                }*/
     }
 
 }
